f80ac06284f45b548d1dbbbdebac1cd6163b261e
Jay Oram <oram.jay@gmail.com>
2022-01-13 10:37:51 +0000

    procfile

--- app.js
+++ app.js
@@ -132,10 +132,9 @@ function getUrl(req) {
  return url;
 }
 
-// /**
-// * Start app
-// */
-// app.listen(3000, function() {
-//  console.log("Listening on port 3000");
-//  console.log(defaults)
-// });
+/**
+* Start app
+*/
+app.listen(app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."))
+    );
